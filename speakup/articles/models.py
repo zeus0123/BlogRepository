4653c26c8517f43e0e63bf1bd009b1d4a8a0ab36
trishanghosh-PC <ghoshtrishan73@gmail.com>
2018-04-08 13:24:51 +0530

    Migrations

--- speakup/articles/models.py
+++ speakup/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug  = models.CharField(max_length=100)
+    body  = models.TextField(max_length=2000)
+    date  = models.DateTimeField(auto_now_add=True)
+    # will add more fields later   
+
+
+    def __str__(self):
+        return self.title
